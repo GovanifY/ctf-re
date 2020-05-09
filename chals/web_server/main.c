e65aec7a8a7be3dda8efabcfd3fae818c7e99340
Gauvain "GovanifY" Roussel-Tarbouriech <gauvain@govanify.com>
2020-05-09 03:18:26 +0200

    SLEEP

--- chals/web_server/main.c
+++ chals/web_server/main.c
@@ -1,23 +1,9 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 
-void main(){
+int main(int argc, char *argv[]) {
+    setreuid(geteuid(), geteuid());
+    setregid(getegid(), getegid());
+    return system("python3 web_server.py");
+}
 
-  setvbuf(stdout, NULL, _IONBF, 0);
-  
-  // Set the gid to the effective gid
-  // this prevents /bin/sh from dropping the privileges
-  gid_t gid = getegid();
-  setresgid(gid, gid, gid);
-    system("python3 web_server.py");
-    }
