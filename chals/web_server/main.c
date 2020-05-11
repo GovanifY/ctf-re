b8e37e04a5f7ef30d62d6ec10c6da4983405f96a
Gauvain "GovanifY" Roussel-Tarbouriech <gauvain@govanify.com>
2020-05-11 02:25:49 +0200

    fix web_server bypass

--- chals/web_server/main.c
+++ chals/web_server/main.c
@@ -1,9 +1,16 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include <linux/limits.h>
+#include <libgen.h>
 
 int main(int argc, char *argv[]) {
     setreuid(geteuid(), geteuid());
     setregid(getegid(), getegid());
+    // ensure CWD is the current folder path to avoid using the SUID bit on
+    // another script
+    char buf[PATH_MAX];
+    readlink("/proc/self/exe", buf, PATH_MAX);
+    chdir(dirname(buf));
     return system("python3 web_server.py");
 }
 
